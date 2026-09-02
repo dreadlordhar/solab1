7796e4772b91338e69831336a1edddabb19764a0
sasa01fr <alexandrucosciug71@gmail.com>
2026-09-02 12:53:23 +0300

    Adaugarea librariilor si realizarea clasei pomodoroservice

--- CosciugMura/Promodoro/untitled/src/PomodoroService.java
+++ CosciugMura/Promodoro/untitled/src/PomodoroService.java
@@ -1,2 +1,12 @@
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 public class PomodoroService {
+private Timer timerPeriodic;
+private Timer timerDelay;
+private Timer timerOraFixa;
+
+public interface TimerCallback{
+    
+}
 }
