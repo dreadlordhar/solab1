7eb458144cd0d2fcfc4a7ee917a9295329a78be9
sasa01fr <alexandrucosciug71@gmail.com>
2026-09-10 12:09:17 +0300

    adaugarea interfatei si a unei functii

--- CosciugMura/Promodoro/untitled/src/PomodoroService.java
+++ CosciugMura/Promodoro/untitled/src/PomodoroService.java
@@ -7,6 +7,12 @@ private Timer timerDelay;
 private Timer timerOraFixa;
 
 public interface TimerCallback{
-    
+    void onTick(int secondsLeft);
+    void onDelayComplete(String msg);
+    void onExactTimeReached(String msg);
 }
+public void startPomodoro(int totalSeconds, TimerCallback callback) {
+        if (timerPeriodic != null) {
+            timerPeriodic.cancel();
+        }
 }
