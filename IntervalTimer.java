eb83e249e0d6f8db0b176e3fd49f88a046a8cc96
evelinanazarco11-prog <evelina.nazarco.11@gmail.com>
2026-09-11 23:11:04 +0300

    Adaug timer cu interval de timp

---
+++ IntervalTimer.java
@@ -0,0 +1,23 @@
+import  java.util.Timer;
+import java.util.TimerTask;
+import java.awt.Toolkit;
+
+class IntervalTimer extends TimerTask{
+    int secunde;
+    Timer timer;
+    IntervalTimer(int secunde) {
+        this.secunde = secunde;
+    }
+  @Override
+ public void run(){
+      System.out.println("Timpul a expirat!");
+      Toolkit.getDefaultToolkit().beep();
+  }
+public void start(){
+Timer timer = new Timer();
+timer.schedule(this,secunde *1000);
+}
+public void stop(){
+        timer.cancel();
+    }
+}
