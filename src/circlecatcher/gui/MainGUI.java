bcc78c6ed6adf906195e46c9fc7a9264b4c2d840
DesuDaen <daen.linux@proton.me>
2026-09-13 19:30:32 +0300

    Added center and right panel to the GUI app

--- src/circlecatcher/gui/MainGUI.java
+++ src/circlecatcher/gui/MainGUI.java
@@ -10,15 +10,30 @@ public class MainGUI {
 
         frame = new JFrame("Hello World Java Swing");
 
+        // create right panel
+        JPanel rightPanel = new JPanel();
+        rightPanel.setPreferredSize(new Dimension(300, 0));
+        rightPanel.setBackground(Color.LIGHT_GRAY);
+
+        // add rightPanel to the frame
+        frame.getContentPane().add(rightPanel, BorderLayout.EAST);
+
+        // create center panel
+        JPanel centerPanel = new JPanel();
+        centerPanel.setBackground(Color.CYAN);
+
+        // add centerPanel to the frame
+        frame.getContentPane().add(centerPanel, BorderLayout.CENTER);
+
         // set frame site
-        frame.setMinimumSize(new Dimension(800, 600));
+        frame.setMinimumSize(new Dimension(1600, 900));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // center the JLabel
         JLabel lblText = new JLabel("Hello World!", SwingConstants.CENTER);
 
         // add JLabel to JFrame
-        frame.getContentPane().add(lblText);
+        centerPanel.add(lblText);
 
     }
 
