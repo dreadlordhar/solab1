b64d29c2bbbc43074563f191735f9b24630f986b
DesuDaen <daen.linux@proton.me>
2026-09-13 20:39:31 +0300

    Added 3 mini panels to host the buttons and labels of control in the right panel

--- src/circlecatcher/gui/MainGUI.java
+++ src/circlecatcher/gui/MainGUI.java
@@ -14,6 +14,8 @@ public class MainGUI {
         JPanel rightPanel = new JPanel();
         rightPanel.setPreferredSize(new Dimension(300, 0));
         rightPanel.setBackground(Color.LIGHT_GRAY);
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS)); // stacks vertically
+        rightPanel.setBorder(BorderFactory.createEmptyBorder(20, 10, 10, 10));
 
         // add rightPanel to the frame
         frame.getContentPane().add(rightPanel, BorderLayout.EAST);
@@ -25,15 +27,43 @@ public class MainGUI {
         // add centerPanel to the frame
         frame.getContentPane().add(centerPanel, BorderLayout.CENTER);
 
+        // create three mini panels to the top of the right panel to host the buttons and the labels
+        Dimension dimPanel = new Dimension(250, 100);
+        // ... timerPanel
+        JPanel timerPanel = new JPanel();
+        timerPanel.setPreferredSize(dimPanel);
+        timerPanel.setMaximumSize(dimPanel);
+        timerPanel.setBackground(Color.YELLOW);
+
+
+        // ... periodPanel
+        JPanel periodPanel = new JPanel();
+        periodPanel.setPreferredSize(dimPanel);
+        periodPanel.setMaximumSize(dimPanel);
+        periodPanel.setBackground(Color.YELLOW);
+
+
+        // ... startPanel
+        JPanel startPanel = new JPanel();
+        startPanel.setPreferredSize(dimPanel);
+        startPanel.setMaximumSize(dimPanel);
+        startPanel.setBackground(Color.YELLOW);
+
+
+        // add them to rightPanel
+        rightPanel.add(timerPanel);
+        rightPanel.add(Box.createVerticalStrut(25)); // 25px gap
+        rightPanel.add(periodPanel);
+        rightPanel.add(Box.createVerticalStrut(25));
+        rightPanel.add(startPanel);
+
+
         // set frame site
         frame.setMinimumSize(new Dimension(1600, 900));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // center the JLabel
-        JLabel lblText = new JLabel("Hello World!", SwingConstants.CENTER);
+        // FROM HERE WE ADD ONLY COMPONENTS TO EXISTENT PANELS
 
-        // add JLabel to JFrame
-        centerPanel.add(lblText);
 
     }
 
